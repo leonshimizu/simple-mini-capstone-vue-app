fabc7bc58a2da320bea11a8e109c44f372418390
“leonshimizu” <lmshimizu@gmail.com>
2021-11-09 18:47:57 -0800

    products create function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,13 @@
     <h1>{{ message }}</h1>
     <h1>{{ firstMessage }}</h1>
     <button v-on:click="doSomething">Doing Something</button>
+    <hr>
+    <h2>Here are all the Products:</h2>
+    <button v-on:click="createFunction">Create Product</button>
+    <p><input type="text" v-model="name"></p>
+    <p><input type="text" v-model="price"></p>
+    <p><input type="text" v-model="description"></p>
+    <p><input type="text" v-model="supplier_id"></p>
     <ol>
       <li v-for="product in products" v-bind:key="product.id">
         {{ product }}
@@ -21,7 +28,11 @@ export default {
     return {
       message: "Welcome to Vue.js!",
       firstMessage: "Hello World",
-      products: []
+      products: [],
+      name: "Name",
+      price: "Price",
+      description: "Description",
+      supplier_id: "Supplier ID"
     };
   },
   created: function() {
@@ -39,6 +50,19 @@ export default {
           console.log(response.data);
           this.products = response.data;
         });
+    },
+    createFunction: function() {
+      console.log("in the create function");
+      axios
+        .post('http://localhost:3000/products', {
+          name: this.name,
+          price: this.price,
+          description: this.description,
+          supplier_id: this.supplier_id
+        }).then(response => {
+          console.log(response.data);
+          this.products.push(response.data);
+        })
     }
   }
 };
