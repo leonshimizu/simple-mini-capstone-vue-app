1078678270a0ad2e0130bde7f45c7fd4fc5f125d
“leonshimizu” <lmshimizu@gmail.com>
2021-11-09 17:14:02 -0800

    updated home.vue with single component and tested hot reload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <h1>{{ firstMessage }}</h1>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style></style>
 
+<script>
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      firstMessage: "Hello World"
+    };
   },
+  created: function() {},
+  methods: {}
 };
 </script>
