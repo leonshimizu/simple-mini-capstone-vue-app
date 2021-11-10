264db955b11a5c29373c077d0d2168b726aaf5c1
“leonshimizu” <lmshimizu@gmail.com>
2021-11-09 18:04:25 -0800

    added index action for products from mini-capstone (rails) and removed prettier

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,23 +3,42 @@
     <h1>{{ message }}</h1>
     <h1>{{ firstMessage }}</h1>
     <button v-on:click="doSomething">Doing Something</button>
+    <ol>
+      <li v-for="product in products" v-bind:key="product.id">
+        {{ product }}
+      </li>
+    </ol>
+    <!-- <p>{{ products }}</p> -->
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from 'axios';
 export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
-      firstMessage: "Hello World"
+      firstMessage: "Hello World",
+      products: []
     };
   },
-  created: function() {},
+  created: function() {
+    this.indexFunction();
+  },
   methods: {
     doSomething: function() {
       console.log("doing something");
+    },
+    indexFunction: function() {
+      console.log("in the index function");
+      axios
+        .get('http://localhost:3000/products')
+        .then(response => {
+          console.log(response.data);
+          this.products = response.data;
+        });
     }
   }
 };
